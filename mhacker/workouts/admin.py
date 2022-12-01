c455d14ca90d23ddbd9998020b3c28d1a7311e9a
tmkerley <70177694+tmkerley@users.noreply.github.com>
2022-11-30 20:19:36 -0700

    html changes

--- mhacker/workouts/admin.py
+++ mhacker/workouts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-from .models import 
+from .models import Exercise
+
+class ExerciseListAdmin(admin.TabularInline):
+    name = Exercise.name
+    muscles = Exercise.muscles
+    equipment = Exercise.equipment
+    aType = Exercise.aType
+
+admin.site.register(Exercise, ExerciseListAdmin)

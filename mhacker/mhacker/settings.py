e15b63606fb661c2882656ab84bc73deb9862045
tmkerley <70177694+tmkerley@users.noreply.github.com>
2022-12-02 12:26:25 -0700

    Server setup

--- mhacker/mhacker/settings.py
+++ mhacker/mhacker/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'mhacker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.postgresql',
+        'OPTIONS': {
+            'service': 'mhacker_db_service',
+            'passfile': '.mhacker_db_pgpass',
+        },
     }
 }
 
